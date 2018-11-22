581fc39a66b60cbbbc74708cd5bc1af0ad441055
nmkolp <45236473+nmkolp@users.noreply.github.com>
2018-11-22 07:19:21 +0200

    Arguments rework

--- deadfish.py
+++ deadfish.py
@@ -5,65 +5,85 @@ class DeadfishError(Exception):
         print(message)
         sys.exit(1)
 
-prog = sys.argv[sys.argv.index('-p')+1] if '-p' in sys.argv else None
-strict = '-strict' in sys.argv or '-s' in sys.argv
-strict_overflow = '-overflow' in sys.argv or '-o' in sys.argv
+prog = None
+if '-p' in sys.argv:
+    prog = sys.argv[sys.argv.index('-p')+1]
+
+variant_tilde = True
+if '-v' in sys.argv:
+    variant_tilde = not ('o' in sys.argv[sys.argv.index('-v')+1] and not ('t' in sys.argv[sys.argv.index('-v')+1]))
+
+cmd_original = True
+cmd_xkdc = False
+if '-c' in sys.argv:
+    cmd_original = 'o' in sys.argv[sys.argv.index('-c')+1]
+    cmd_xkdc = 'x' in sys.argv[sys.argv.index('-c')+1]
+
+wimp_errors = False
+wimp_overflow = False
+wimp_true_overflow = False
+wimp_reset = False
+if '-w' in sys.argv:
+    wimp_errors = 'e' in sys.argv[sys.argv.index('-w')+1]
+    wimp_overflow = 'o' in sys.argv[sys.argv.index('-w')+1]
+    wimp_true_overflow = 'O' in sys.argv[sys.argv.index('-w')+1]
+    wimp_reset = 'r' in sys.argv[sys.argv.index('-w')+1]
 
 def execute(acc, prog, i=0, recursion=False):
     while i < len(prog):
         char = prog[i]
-        if char in ['i','x']:
+        if (cmd_original and char == 'i') or (cmd_xkdc and char == 'x'):
             acc += 1
         elif char == 'd':
             acc -= 1
-        elif char in ['s', 'k']:
+        elif (cmd_original and char == 's') or (cmd_xkdc and char == 'k'):
             acc *= acc
-        elif char == 'h':
+        elif variant_tilde and ((cmd_original and char == 'h') or (not recursion and cmd_xkdc and char == 'D')):
             print('\nLong live the fish!')
             sys.exit(0)
-        elif char == 'r':
+        elif wimp_reset and char == 'r':
             acc = 0
-        elif char == 'w':
+        elif variant_tilde and ((cmd_original and char == 'w') or (cmd_xkdc and char == 'K')):
             print('Hello, World!', end='')
-        elif recursion and char == '}':
+        elif variant_tilde and recursion and ((cmd_original and char == '}') or (cmd_xkdc and char == 'D')):
             return [acc, i]
-        elif char == '{':
+        elif variant_tilde and ((cmd_original and char == '{') or (cmd_xkdc and char == 'X')):
             for _ in range(0, 9):
                 state = execute(acc, prog, i + 1, True)
                 acc = state[0]
             state = execute(acc, prog, i + 1, True)
             acc = state[0]
             i = state[1]
-        elif char == '(':
+        elif variant_tilde and ((cmd_original and char == '(') or (cmd_xkdc and char == 'x')):
             if acc == 0:
                 i += 1
                 while i < len(prog):
                     char = prog[i]
-                    if char == ')':
+                    if (cmd_original and char == ')') or (cmd_xkdc and char == 'D'):
                         break
                     i += 1
                 if i >= len(prog):
                     break
-        elif not char in ['o', 'c']:
-            if strict:
+        elif not ((variant_tilde and cmd_original and char in ['o', 'c']) or (variant_tilde and cmd_xkdc and char in ['C', 'c']) or (not variant_tilde and cmd_original and char == 'o') or (not variant_tilde and cmd_xkdc and char == 'c')):
+            if wimp_errors:
                 raise DeadfishError('\nInvalid character {} at index {}'.format(char, i))
             else:
                 print('\n', end='')
-        if acc == -1 or acc == 256:
-            if strict_overflow:
+        if (acc == -1 or acc == 256) or (wimp_true_overflow and (acc < 0 or acc > 255)):
+            if wimp_overflow:
                 raise DeadfishError('\nOverflow at char {} (Index {}) accumulator = {}'.format(char, i, acc))
             else:
                 acc = 0
-        if char == 'o':
+        if (cmd_original and char == 'o') or (cmd_xkdc and char == 'c' and not variant_tilde) or (cmd_xkdc and char == 'C' and variant_tilde):
             print(acc, end='')
-        elif char == 'c':
+        elif variant_tilde and char == 'c':
             print(chr(acc), end='')
         i += 1
     return [acc, i]
 
 acc = 0
 
-print('Deadfish~ interpreter version 0.11')
+print('Deadfish~ interpreter version 0.12')
 
 while True:
     if not bool(prog):
